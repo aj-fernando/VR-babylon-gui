6870ba0e0b1c413b4e86a839b4d183a3d7f702df
AJ <aj.fernando@holoplot.com>
2020-01-28 18:24:56 +0100

    Add Vue-babylonjs & VueAxios to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vb from 'vue-babylonjs';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(vb);
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
