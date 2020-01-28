8dd43258c463a9b77fc38a02be5df53464cf0788
AJ <aj.fernando@holoplot.com>
2020-01-28 18:33:01 +0100

    Add initial SecondBabylon.vue (Err: cannot read property MeshButton3D)
    - importing BABYLON through 'vue-babylonjs'
    - no anchor, no GUI3DManager, addControl() and linkToTransformNode()

---
+++ src/components/SecondBabylon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Scene @scene="onScene">
+      <Camera @camera="onCamera"></Camera>
+      <HemisphericLight diffuse="#FFFFFF"></HemisphericLight>
+    </Scene>
+  </div>
+</template>
+
+<script>
+import { BABYLON, Scene } from "vue-babylonjs";
+
+export default {
+  /* eslint-disable */
+  components: { BABYLON, Scene },
+  name: "SecondBabylon",
+  data: () => ({
+    testBox: null,
+    testButton: null,
+    vrHelper: null
+  }),
+  mounted() {},
+  methods: {
+    onCamera(camera) {},
+    testMethod(scene) {
+      console.log(scene);
+      this.testBox = BABYLON.Mesh.CreateBox("b1", 1.0, scene);
+      this.testBox.name = "test1";
+      this.testButton = BABYLON.GUI.MeshButton3D(this.testBox, "name");
+    },
+    onScene(scene) {
+      this.vrHelper = scene.createDefaultVRExperience({
+        createDeviceOrientationCamera: false
+      });
+      /* eslint-disable */
+      console.log(this.vrHelper);
+      this.testMethod(scene);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
