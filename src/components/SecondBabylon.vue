46ca3bb29907d21ccdbb912c29fecc4570d55b25
AJ <aj.fernando@holoplot.com>
2020-02-04 19:23:28 +0100

    Import mesh in script through Babylon.js methods

--- src/components/SecondBabylon.vue
+++ src/components/SecondBabylon.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Scene @scene="onScene">
-      <Camera @camera="onCamera"></Camera>
+      <!-- <Camera @camera="onCamera"></Camera> -->
       <HemisphericLight diffuse="#FFFFFF"></HemisphericLight>
     </Scene>
   </div>
@@ -9,7 +9,8 @@
 
 <script>
 import * as BABYLON from "babylonjs";
-import * as GUI from "babylonjs-gui";
+//import * as GUI from "babylonjs-gui";
+import "babylonjs-loaders";
 
 export default {
   name: "SecondBabylon",
@@ -18,7 +19,10 @@ export default {
     anchorMesh: null,
     testBox: null,
     testButton: null,
-    vrHelper: null
+    testMesh: null,
+    vrHelper: null,
+    publicPath: `${process.env.BASE_URL}`,
+    meshPath: null
   }),
   mounted() {},
   methods: {
@@ -27,27 +31,51 @@ export default {
     },
     testMethod(scene) {
       console.log("Scene:", scene);
+
+      //Adding an Arc Rotate Camera
+      var camera = new BABYLON.ArcRotateCamera(
+        "Camera",
+        0,
+        0.8,
+        100,
+        BABYLON.Vector3.Zero(),
+        scene
+      );
+      camera.attachControl(scene.getEngine().getRenderingCanvas(), false);
+
       this.testBox = new BABYLON.Mesh.CreateBox("b1", 1.0, scene);
       console.log("TestBox:", this.testBox);
       this.testBox.name = "test1";
 
-      // add a 3d button
-      this.testButton = new GUI.MeshButton3D(this.testBox, "name");
-      // Now you can start adding controls to the GUI manager rootContainer
-      this.gui3DManager.addControl(this.testButton);
-      //  attach a control to a mesh or tranform node from your scene
-      this.testButton.linkToTransformNode(this.anchorMesh);
+      // The first parameter can be used to specify which mesh to import. Here we import all meshes
+      console.log(this.publicPath);
+      this.meshPath = this.publicPath + "skull.babylon";
+      console.log(this.meshPath);
+
+      BABYLON.SceneLoader.ImportMesh("", this.meshPath, "", scene, function(
+        newMeshes
+      ) {
+        // Set the target of the camera to the first imported mesh
+        camera.target = newMeshes[0];
+      });
+
+      // // add a 3d button
+      // this.testButton = new GUI.MeshButton3D(this.testBox, "name");
+      // // Now you can start adding controls to the GUI manager rootContainer
+      // this.gui3DManager.addControl(this.testButton);
+      // //  attach a control to a mesh or tranform node from your scene
+      // this.testButton.linkToTransformNode(this.anchorMesh);
     },
     onScene(scene) {
-      this.vrHelper = scene.createDefaultVRExperience({
-        createDeviceOrientationCamera: false
-      });
-      console.log("VR Helper:", this.vrHelper);
+      // this.vrHelper = scene.createDefaultVRExperience({
+      //   createDeviceOrientationCamera: false
+      // });
+      // console.log("VR Helper:", this.vrHelper);
 
-      // create anchorMesh : https://www.babylonjs-playground.com/#2YZFA0#2
-      this.anchorMesh = new BABYLON.AbstractMesh("anchorMesh", scene); // new BABYLON.TransformNode("");
-      // create the 3D GUI manager
-      this.gui3DManager = new GUI.GUI3DManager(scene);
+      // // create anchorMesh : https://www.babylonjs-playground.com/#2YZFA0#2
+      // this.anchorMesh = new BABYLON.AbstractMesh("anchorMesh", scene); // new BABYLON.TransformNode("");
+      // // create the 3D GUI manager
+      // this.gui3DManager = new GUI.GUI3DManager(scene);
 
       this.testMethod(scene);
     }
